601ee95811f4d4bb038754dd7ed6c6ebaff18dc5
Malik8008 <tu60prbs1@code.edu.az>
2022-03-30 13:44:52 +0400

    Collection-Task-Completed

---
+++ Generic-Collection-Task2/Generic-Collection-Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace Generic_Collection_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList sortlist = new SortedList();
+
+            sortlist.Add(1995, "PHP");
+            sortlist.Add(1966, "JOSS");
+            sortlist.Add(1978, "SQL");
+            sortlist.Add(1970, "PASCAL");
+            sortlist.Add(1980, "C++");
+
+            foreach (DictionaryEntry collec in sortlist)
+            {
+                Console.WriteLine(collec.Key +" "+ collec.Value);
+            }
+
+
+
+        }
+    }
+}
